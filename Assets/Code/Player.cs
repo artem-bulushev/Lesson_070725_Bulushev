760c65ca764260b1714f642929c6793d2900f068
artem-bulushev <bulushiev93@mail.ru>
2025-08-29 13:11:28 +0300

    Изменения: - убрал лишние ссылки (строки 1,2) - убрал лишнюю строку (19)

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Code
@@ -16,6 +14,5 @@ namespace Code
         {
             return (int)(a + b);
         }
-
     }
 }
